b2c960ad61b4ff8c964444adf0c7e73ca495720e
Rob <robert.sinclair@my.jcu.edu.a>
2020-03-31 22:30:49 +1000

    Fleshed out settings menu with intents, created reset and pause buttons which change the main menu.

--- app/src/main/java/com/example/stepaholic/MainActivity.java
+++ app/src/main/java/com/example/stepaholic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.stepaholic;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,15 +11,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView stepsTextView;
+    private TextView milesTextView;
+    private TextView kilometreTextView;
     boolean userRunning = false;
     SensorManager sensorManager = null;
-    float steps;
+    float steps = 0;
     float miles = steps / 2000;
     float kilometres = steps / 1500;
     ImageButton settingsButton;
@@ -34,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 settingsOpen();
             }
         });
+        stepsTextView = findViewById(R.id.stepsDisplay);
+        milesTextView = findViewById(R.id.mileDisplay);
+        kilometreTextView = findViewById(R.id.kiloDisplay);
     }
 
     @Override
@@ -59,12 +65,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
     if (userRunning){
         steps = event.values[0];
-        TextView stepsTextview = findViewById(R.id.stepsDisplay);
-        stepsTextview.setText(String.format("%s", steps));
-        TextView milesTextview = findViewById(R.id.mileDisplay);
-        milesTextview.setText(String.format("%s", miles));
-        TextView kilometreTextview = findViewById(R.id.kiloDisplay);
-        kilometreTextview.setText(String.format("%s", kilometres));
+        stepsTextView.setText(String.format("%s", steps));
+        milesTextView.setText(String.format("%s", miles));
+        kilometreTextView.setText(String.format("%s", kilometres));
     }
     }
 
@@ -74,6 +77,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
     public void settingsOpen(){
         Intent intent = new Intent(this,SettingsActivity.class);
-        startActivity(intent);
+        intent.putExtra("",steps);
+        intent.putExtra("",userRunning);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            if (resultCode == RESULT_OK){
+                steps = data.getIntExtra("result",0);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/stepaholic/SettingsActivity.java
+++ app/src/main/java/com/example/stepaholic/SettingsActivity.java
@@ -2,13 +2,53 @@ package com.example.stepaholic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    int steps;
+    boolean userRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Intent intent = getIntent();
+        steps = intent.getIntExtra("Steps",0);
+        userRunning = intent.getBooleanExtra("Running",true);
+
+        Button resetButton = findViewById(R.id.resetButton);
+        Button pauseButton = findViewById(R.id.pauseButton);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetSteps();
+                Intent resetIntent = new Intent();
+                resetIntent.putExtra("reset",steps);
+                setResult(RESULT_OK,resetIntent);
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+                Intent pauseIntent = new Intent();
+                pauseIntent.putExtra("reset",steps);
+                setResult(RESULT_OK,pauseIntent);
+            }
+        });
+    }
+    public void resetSteps(){
+        steps = 0;
+    }
+    public void pause(){
+        if (userRunning){
+            Toast.makeText(this, "Pausing", Toast.LENGTH_SHORT).show();
+            userRunning = false;
+        }else Toast.makeText(this, "Unpausing", Toast.LENGTH_SHORT).show();
     }
 }
